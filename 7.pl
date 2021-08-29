155b9f433854e6456415ff17da858895c2b85e59
allenalvin333 <allenalvin333@gmail.com>
2021-08-29 11:44:16 +0530

    programs

--- 7.pl
+++ 7.pl
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/matching-x-repetitions/problem
 
-$Regex_Pattern = '^[A-Za-z02468]{40}[13579\\s]{5}$';
+$Regex_Pattern = '^[A-Za-z02468]{40}[13579\s]{5}$';
 
 $Test_String = <STDIN> ;
 if($Test_String =~ /$Regex_Pattern/){
