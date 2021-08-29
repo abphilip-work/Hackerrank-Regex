4d26a797834a656fefdb11c62aa4d3b82eef153b
allenalvin333 <allenalvin333@gmail.com>
2021-08-29 11:02:00 +0530

    programs

---
+++ 7.pl
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/matching-x-repetitions/problem
+
+$Regex_Pattern = '^[a-zA-Z02468]{40}[13579\\s]{5}$';
+
+$Test_String = <STDIN> ;
+if($Test_String =~ /$Regex_Pattern/){
+    print "true";
+} else {
+    print "false";
+}
