eee22b2af22edb42ef2cf00da453e248d23ab3b7
allenalvin333 <allenalvin333@gmail.com>
2021-08-29 11:46:08 +0530

    programs

--- 8.pl
+++ 8.pl
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/matching-x-y-repetitions/problem
 
-$Regex_Pattern = '^\\d{1,2}[a-zA-Z]{3,}\\.{0,3}$';
+$Regex_Pattern = '^[0-9]{1,2}[A-Za-z]{3,}\.{0,3}$';
 
 $Test_String = <STDIN> ;
 if($Test_String =~ /$Regex_Pattern/){
