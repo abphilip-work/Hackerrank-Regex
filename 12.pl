e5f7cbfb64514818d9cc8902c1930a069e82cb34
allenalvin333 <allenalvin333@gmail.com>
2021-08-30 16:26:18 +0530

    programs

---
+++ 12.pl
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/matching-specific-characters/problem
+
+$Regex_Pattern = '^[123][120][xs0][30Aa][xsu][\.,]$';
+
+$Test_String = <STDIN> ;
+if($Test_String =~ /$Regex_Pattern/){
+    print "true";
+} else {
+    print "false";
+}
