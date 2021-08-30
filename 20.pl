b97c2fb0bb84e3872dd593c98b8e813d5721815b
allenalvin333 <allenalvin333@gmail.com>
2021-08-30 16:59:54 +0530

    programs

---
+++ 20.pl
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/branch-reset-groups/problem
+
+$Regex_Pattern = '^\d{2}(?|(:)|(.)|(-{3})|(-))\d{2}\1\d{2}\1\d{2}$';
+
+$Test_String = <STDIN> ;
+if($Test_String =~ /$Regex_Pattern/){
+    print "true";
+} else {
+    print "false";
+}
