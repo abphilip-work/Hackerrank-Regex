360c16155c0b38f83e95ef3053c68a8d3e82ba34
allenalvin333 <allenalvin333@gmail.com>
2021-08-30 16:44:34 +0530

    programs

---
+++ 17.pl
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/alternative-matching/problem
+
+$Regex_Pattern = '^(Mr|Mrs|Ms|Dr|Er)\.[A-Za-z]+$';
+
+$Test_String = <STDIN> ;
+if($Test_String =~ /$Regex_Pattern/){
+    print "true";
+} else {
+    print "false";
+}
