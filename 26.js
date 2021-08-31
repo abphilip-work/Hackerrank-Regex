06c28ad777db72de1d357f6c741625e9d397aa19
allenalvin333 <allenalvin333@gmail.com>
2021-08-31 14:56:31 +0530

    programs

---
+++ 26.js
@@ -0,0 +1,19 @@
+function processData(input) {
+    const r = /<\s*([a-z][a-z0-9]*)[^>]*>/ig;
+    const a = new Set(); 
+    while (match = r.exec(input)) {
+        a.add(match[1]);
+    }
+    console.log(Array.from(a).sort().join(';'));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
