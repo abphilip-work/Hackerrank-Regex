00ac9dacdb06a98c745f063779248fc5638283ca
allenalvin333 <allenalvin333@gmail.com>
2021-09-01 08:01:59 +0530

    programs

--- 27.js
+++ 27.js
@@ -23,3 +23,12 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
+
+/*
+import re
+n = int(input())
+text = "\n".join(input() for _ in range(n))
+t = int(input())
+for _ in range(t):
+    print(len(re.findall(r'\B(%s)\B' % input().strip(),text)))
+*/
