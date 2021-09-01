4fc748328853291c5ee70d938de33890d3554eac
allenalvin333 <allenalvin333@gmail.com>
2021-09-01 08:02:42 +0530

    programs

--- 27.js
+++ 27.js
@@ -26,9 +26,6 @@ process.stdin.on("end", function () {
 
 /*
 import re
-n = int(input())
-text = "\n".join(input() for _ in range(n))
-t = int(input())
-for _ in range(t):
-    print(len(re.findall(r'\B(%s)\B' % input().strip(),text)))
+text = "\n".join(input() for _ in range(int(input())))
+for _ in range(int(input())): print(len(re.findall(r'\B(%s)\B' % input().strip(),text)))
 */
