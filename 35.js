4c8a2c296a32ddde10c23da7e5bee0649e8551f1
allenalvin333 <allenalvin333@gmail.com>
2021-09-01 08:48:37 +0530

    programs

--- 35.js
+++ 35.js
@@ -1,5 +1,23 @@
 // https://www.hackerrank.com/challenges/find-hackerrank/problem
 
+function processData(input) {
+    let [n, ...s] = input.trim().split('\n')
+    s.forEach(i => {
+    let [, start='', end=''] = i.match(/(.)*hackerrank(.)*/) || []
+    console.log([[0, 1], [2, -1]][start.length][end.length])
+    })
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
 
 /*
 import re
