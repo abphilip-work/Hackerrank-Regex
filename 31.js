f2ee815f634a0eb23503d4e62c28a2746ae2bc75
allenalvin333 <allenalvin333@gmail.com>
2021-09-01 08:12:59 +0530

    programs

--- 31.js
+++ 31.js
@@ -1,1 +1,23 @@
-// 
+// https://www.hackerrank.com/challenges/hackerrank-tweets/problem
+
+function processData(input) {
+    console.log(input.match(/hackerrank/ig).length);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
+
+/*
+import re
+i = ' '.join([input() for _ in range(int(input()))])
+regex = r'hackerrank'
+print(len(re.findall(regex, i, re.IGNORECASE)))
+*/
